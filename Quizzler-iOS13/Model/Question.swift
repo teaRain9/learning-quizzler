37ce9ad7c75233572f15ba6bf99457427267a4eb
Adel <adel.ht@gmail.com>
2023-06-23 13:36:32 -0700

    completed

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Adel on 2023-06-23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
